084f687bd0ceb08db5de6ce1dd615f4d85861639
Nicodemus Karisa <nkarisa@ke.ci.org>
2024-08-13 21:49:26 +0300

    Added helpers

---
+++ church-manager/app/Helpers/church_helper.php
@@ -0,0 +1,15 @@
+<?php 
+
+use Hashids\Hashids;
+
+if(!function_exists('hash_id')){
+    function hash_id($id, $dir = "encode") {
+        $hashids = new Hashids("Church-Manager");
+
+        if($dir == 'encode'){
+            return $hashids->encode($id);
+        }else{
+           return $hashids->decode($id);
+        }
+    }
+}
