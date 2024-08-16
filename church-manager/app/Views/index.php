cb615f20b5a6f2e2819ec507922fb40b4f3c33aa
Nicodemus Karisa <nkarisa@ke.ci.org>
2024-08-16 12:38:16 +0300

    Remoeved scripts

--- church-manager/app/Views/index.php
+++ church-manager/app/Views/index.php
@@ -87,8 +87,8 @@
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/js/rickshaw/rickshaw.min.css"> -->
 
 	<!-- Bottom scripts (common) -->
-	<script src="<?php echo base_url(); ?>assets/js/gsap/TweenMax.min.js"></script>
-	<script src="<?php echo base_url(); ?>assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+	<!-- <script src="<?php echo base_url(); ?>assets/js/gsap/TweenMax.min.js"></script> -->
+	<!-- <script src="<?php echo base_url(); ?>assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script> -->
 	<script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/joinable.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/resizeable.js"></script>
