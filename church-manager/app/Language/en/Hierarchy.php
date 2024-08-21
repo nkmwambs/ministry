a2acb675e455e17fb3a559cd5348edb883eb4284
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-08-21 12:30:08 +0300

    added langauge translation to hierarchy

--- church-manager/app/Language/en/Hierarchy.php
+++ church-manager/app/Language/en/Hierarchy.php
@@ -8,7 +8,7 @@ return [
     'select_denomination' => 'Select Denomination',
 
     'hierarchy_action' => 'Action',
-    'hierarchy_name' => 'Member Name',
+    'hierarchy_name' => 'Name',
     'hierarchy_denomination_id' => 'Denomination Name',
     'hierarchy_description' => 'Description',
     'hierarchy_level' => 'Level',
