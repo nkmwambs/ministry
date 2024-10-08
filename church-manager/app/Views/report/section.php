88e56302737e2325e9e0f0f176eb7c4df267e445
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-10-08 12:16:57 +0300

    report types is complete, started reports

---
+++ church-manager/app/Views/report/section.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-md-12">
+        <!-- <div class="panel panel-primary"> -->
+
+            <div class="col-md-8 col-xl-9">
+                <div class="tab-content" id="section_data">
+
+                </div>
+            </div>
+
+        <!-- </div> -->
+    </div>
+</div>
+
+
