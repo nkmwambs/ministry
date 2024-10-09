eef026188947b79f897e0fad5bf459f8a918f846
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-10-09 23:26:11 +0300

    corrected entities add and edit

--- church-manager/app/Views/event/add.php
+++ church-manager/app/Views/event/add.php
@@ -136,17 +136,18 @@ $numeric_meeting_id = hash_id($meeting_id, 'decode');
               <input type="text" class="form-control" name="registration_fees" id="registration_fees" placeholder="Enter Registration Fees">
             </div>
           </div>
-        </form>
 
-        <!-- Dynamically Generated Custom Fields -->
-        <?php foreach ($customFields as $field): ?>
-          <div class="form-group custom_field_container" id="<?= $field['visible']; ?>">
-            <label class="control-label col-xs-4" for="<?= $field['field_name'] ?>"><?= ucfirst($field['field_name']) ?></label>
-            <div class="col-xs-6">
-              <input type="<?= $field['type'] ?>" name="custom_fields[<?= $field['id'] ?>]" id="<?= $field['field_name'] ?>" class="form-control">
+          <!-- Dynamically Generated Custom Fields -->
+          <?php foreach ($customFields as $field): ?>
+            <div class="form-group custom_field_container" id="<?= $field['visible']; ?>">
+              <label class="control-label col-xs-4" for="<?= $field['field_name'] ?>"><?= ucfirst($field['field_name']) ?></label>
+              <div class="col-xs-6">
+                <input type="<?= $field['type'] ?>" name="custom_fields[<?= $field['id'] ?>]" id="<?= $field['field_name'] ?>" class="form-control">
+              </div>
             </div>
-          </div>
-        <?php endforeach; ?>
+          <?php endforeach; ?>
+          
+        </form>
 
       </div>
 
