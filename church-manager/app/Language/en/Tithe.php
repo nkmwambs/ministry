da3993f7eda359bfc71cc4d52236c0ac6bc94fa8
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-10-31 10:50:14 +0300

    WIP

---
+++ church-manager/app/Language/en/Tithe.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'list_tithes' => 'List Tithes',
+    'add_tithe' => 'Add Tithe',
+    'view_tithe' => 'View Tithe',
+    'edit_tithe' => 'Edit Tithe',
+    'delete_tithe' => 'Delete Tithe',
+
+    'tithe_action' => 'Actions',
+    'tithe_member_id' => 'Member Name',
+    'tithe_amount' => 'Amount',
+    // 'tithe_default' => 'Default Role',
+    // 'tithe_denomination_name' => 'Denomination Name',
+
+    'select_member' => 'Select a Member',
+
+    'enter_member_name' => 'Enter Member Name',
+    'enter_tithe_amount' => 'Enter Tithe Amount',
+    'edit_tithe_amount' => 'Edit Tithe Amount',
+
+    'add_role_success' => 'Role added successfully!',
+    'edit_role_success' => 'Role updated successfully!',
+    'delete_role_success' => 'Role deleted successfully!',
+    'delete_role_confirm' => 'Are you sure you want to delete this role?',
+];
