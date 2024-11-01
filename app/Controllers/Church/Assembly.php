2631a539ddf65be71d6272522e84faa5988dd089
Nicodemus Karisa <nkarisa@ke.ci.org>
2024-11-01 22:12:14 +0300

    Separated views according to user type

---
+++ app/Controllers/Church/Assembly.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Church;
+
+use App\Controllers\WebController;
+
+class Assembly extends WebController
+{
+    
+}
---
+++ app/Controllers/Church/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Church;
+
+use App\Controllers\WebController;
+
+class User extends WebController
+{
+    
+}
