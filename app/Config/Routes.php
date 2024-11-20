bf4e0560c6ae54d28b53b40a06b4c02246acab3d
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-11-20 10:52:58 +0300

    WIP

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,58 +9,83 @@ helper('inflector');
 //  */
 
 
-$routes->group("admin", ['namespace' => 'App\Controllers\Admin'],function($routes){
+$routes->group("admin", ['namespace' => 'App\Controllers\Admin'], function ($routes) {
     $featureModel = new \App\Models\FeaturesModel();
     $features = $featureModel->findAll();
-    
+
     foreach ($features as $featureObj) {
         $feature = $featureObj['name'];
         $ucfirst = ucfirst($feature);
         $group = plural($feature);
         $routes->group($group, function ($routes) use ($ucfirst, $group) {
-                $routes->get('list', "$ucfirst::index");
-                $routes->get('list/(:segment)', "$ucfirst::index/$1");
-                $routes->get('add', "$ucfirst::add");
-                $routes->get('view/(:segment)', "$ucfirst::view/$1");
-                $routes->get('view/(:segment)/(:segment)', "$ucfirst::view/$1");
-                $routes->get('edit/(:segment)', "$ucfirst::edit/$1");
-                $routes->get('delete/(:segment)', "$ucfirst::delete/$1");
-                $routes->post('update', "$ucfirst::update");
-                $routes->post('save', "$ucfirst::post");
-                $routes->get('modal/(:segment)/(:segment)', "$ucfirst::modal/$1/$2");
-                $routes->get('modal/(:segment)/(:segment)/(:segment)', "$ucfirst::modal/$1/$2/$3");
-                $routes->post('getFields/(:segment)/(:segment)', "$ucfirst::getBulkActionFields/$1/$2");
-                $routes->post('bulk_edit', "$ucfirst::bulkEdit");
+            $routes->get('list', "$ucfirst::index");
+            $routes->get('list/(:segment)', "$ucfirst::index/$1");
+            $routes->get('add', "$ucfirst::add");
+            $routes->get('view/(:segment)', "$ucfirst::view/$1");
+            $routes->get('view/(:segment)/(:segment)', "$ucfirst::view/$1");
+            $routes->get('edit/(:segment)', "$ucfirst::edit/$1");
+            $routes->get('delete/(:segment)', "$ucfirst::delete/$1");
+            $routes->post('update', "$ucfirst::update");
+            $routes->post('save', "$ucfirst::post");
+            $routes->get('modal/(:segment)/(:segment)', "$ucfirst::modal/$1/$2");
+            $routes->get('modal/(:segment)/(:segment)/(:segment)', "$ucfirst::modal/$1/$2/$3");
+            $routes->post('getFields/(:segment)/(:segment)', "$ucfirst::getBulkActionFields/$1/$2");
+            $routes->post('bulk_edit', "$ucfirst::bulkEdit");
         });
     }
-    
 });
 
 
-$routes->group("church", ['namespace' => 'App\Controllers\Church'],function($routes){
+$routes->group("church", ['namespace' => 'App\Controllers\Church'], function ($routes) {
     $featureModel = new \App\Models\FeaturesModel();
     $features = $featureModel->findAll();
-    
+
     foreach ($features as $featureObj) {
         $feature = $featureObj['name'];
         $ucfirst = ucfirst($feature);
         $group = plural($feature);
         $routes->group($group, function ($routes) use ($ucfirst, $group) {
-                $routes->get('list', "$ucfirst::index");
-                $routes->get('list/(:segment)', "$ucfirst::index/$1");
-                $routes->get('add', "$ucfirst::add");
-                $routes->get('view/(:segment)', "$ucfirst::view/$1");
-                $routes->get('view/(:segment)/(:segment)', "$ucfirst::view/$1");
-                $routes->get('edit/(:segment)', "$ucfirst::edit/$1");
-                $routes->get('delete/(:segment)', "$ucfirst::delete/$1");
-                $routes->post('update', "$ucfirst::update");
-                $routes->post('save', "$ucfirst::post");
-                $routes->get('modal/(:segment)/(:segment)', "$ucfirst::modal/$1/$2");
-                $routes->get('modal/(:segment)/(:segment)/(:segment)', "$ucfirst::modal/$1/$2/$3");
-                $routes->post('getFields/(:segment)/(:segment)', "$ucfirst::getBulkActionFields/$1/$2");
-                $routes->post('bulk_edit', "$ucfirst::bulkEdit");
-                $routes->post('showList', "$ucfirst::showList");
+            $routes->get('list', "$ucfirst::index");
+            $routes->get('list/(:segment)', "$ucfirst::index/$1");
+            $routes->get('add', "$ucfirst::add");
+            $routes->get('view/(:segment)', "$ucfirst::view/$1");
+            $routes->get('view/(:segment)', "$ucfirst::viewMyProfile/$1");
+            $routes->get('view/(:segment)/(:segment)', "$ucfirst::view/$1");
+            $routes->get('edit/(:segment)', "$ucfirst::edit/$1");
+            $routes->get('delete/(:segment)', "$ucfirst::delete/$1");
+            $routes->post('update', "$ucfirst::update");
+            $routes->post('save', "$ucfirst::post");
+            $routes->get('modal/(:segment)/(:segment)', "$ucfirst::modal/$1/$2");
+            $routes->get('modal/(:segment)/(:segment)/(:segment)', "$ucfirst::modal/$1/$2/$3");
+            $routes->post('getFields/(:segment)/(:segment)', "$ucfirst::getBulkActionFields/$1/$2");
+            $routes->post('bulk_edit', "$ucfirst::bulkEdit");
+            $routes->post('showList', "$ucfirst::showList");
         });
+
+        $routes->post('users/profile/account/save', "User::updatePublicInfo");
+        $routes->post('users/profile/account/update_public_info', "User::updatePublicInfo");
+        $routes->post('users/profile/account/update_private_info', "User::updatePrivateInfo");
+
+        $routes->group('users/profile', ['namespace' => 'App\Controllers'], function ($routes) {
+            $routes->get('account/(:segment)', 'User::getAccount/$1');
+            $routes->get('account', 'User::editAccount');
+            $routes->post('account/update_public_info', 'User::updatePrivateInfo');
+            $routes->post('account/update_private_info', 'User::updatePrivateInfo');
+            $routes->post('account/save', 'User::postPrivateInfo');
+            $routes->get('password_reset/(:segment)', "User::passwordReset/$1");
+            $routes->post('verify_password', "User::passwordVerify");
+            $routes->get('email_notifications/(:segment)', "User::emailNotifications/$1");
+            $routes->get('pending_tasks/(:segment)', 'User::pendingTasks/$1');
+            $routes->post('save_task', 'Task::saveTask');
+            $routes->get('privacy/(:segment)', "User::privacy/$1");
+            $routes->get('your_data/(:segment)', "User::yourData/$1");
+            $routes->get('downloadUserData/(:segment)', 'User::downloadUserData/$1');
+            $routes->get('deleteAccount/(:segment)', 'User::deleteAccount/$1');
+            $routes->get('delete_account/(:segment)', "User::deleteAccount/$1");
+        });
+
+        $routes->post('users/update/public/', 'User::updatePublicInfo');
+        $routes->post('users/update/private/', 'User::updatePrivateInfo');
     }
 });
 
@@ -68,8 +93,10 @@ $routes->post('church/member/showList', [App\Controllers\Church\Member::class, "
 $routes->post('church/collection/showList', [App\Controllers\Church\Collection::class, "showList"]);
 $routes->post('church/tithe/showList', [App\Controllers\Church\Tithe::class, "showList"]);
 
+$routes->get('church/users/profile/account/(:segment)', [App\Controllers\Church\User::class,'getAccount/$1']);
 
-$routes->group("",['namespace' => 'App\Controllers\Admin'], function($routes){
+
+$routes->group("", ['namespace' => 'App\Controllers\Admin'], function ($routes) {
     $routes->get('entities/items/(:segment)/(:segment)', "Entity::getParentEntitiesByDenomination/$1/$2");
     $routes->get('entities/lowestEntities/(:segment)', "Entity::getDenominationLowestEntities/$1");
     $routes->get('entities/hierarchy/(:segment)', "Entity::getEntitiesByHierarchyId/$1");
@@ -81,12 +108,12 @@ $routes->group("",['namespace' => 'App\Controllers\Admin'], function($routes){
     $routes->get('roles/get_default_role/(:segment)', 'Role::getDefaultRole/$1');
     $routes->get('features/get_allowable_permission_labels/(:segment)', 'Feature::getAllowablePermissionLabels/$1');
     $routes->post('permissions/update_permission', 'Permission::updatePermission/');
-    
+
     $routes->post('users/profile/account/save', "User::updatePublicInfo");
     $routes->post('users/profile/account/update_public_info', "User::updatePublicInfo");
     $routes->post('users/profile/account/update_private_info', "User::updatePrivateInfo");
-    
-    $routes->group('users/profile', ['namespace' => 'App\Controllers'], function($routes) {
+
+    $routes->group('users/profile', ['namespace' => 'App\Controllers'], function ($routes) {
         $routes->get('account/(:segment)', 'User::getAccount/$1');
         $routes->get('account', 'User::editAccount');
         $routes->post('account/update_public_info', 'User::updatePrivateInfo');
@@ -103,12 +130,12 @@ $routes->group("",['namespace' => 'App\Controllers\Admin'], function($routes){
         $routes->get('deleteAccount/(:segment)', 'User::deleteAccount/$1');
         $routes->get('delete_account/(:segment)', "User::deleteAccount/$1");
     });
-    
-    $routes->post('users/update/public/', 'User::updatePublicInfo'); 
+
+    $routes->post('users/update/public/', 'User::updatePublicInfo');
     $routes->post('users/update/private/', 'User::updatePrivateInfo');
-    
+
     $routes->post('/tasks/updateStatus', 'Task::updateStatus');
-    
+
     $routes->post('denominations/fetchDenominations', 'Denomination::fetchDenominations');
     $routes->post('ministers/fetchMinisters', 'Minister::fetchMinisters');
     $routes->post('ministers/fetchMinisters', 'Minister::fetchMinisters');
@@ -118,24 +145,24 @@ $routes->group("",['namespace' => 'App\Controllers\Admin'], function($routes){
     $routes->post('departments/fetchDepartments', 'Department::fetchDepartments');
     $routes->post('reports/fetchReports', 'Report::fetchReports');
     $routes->post('assemblies/fetchAssemblies', 'Assembly::fetchAssemblies');
-    $routes->post('members/fetchMembers/(:num)','Member::fetchMembers/$1');
-    
-    $routes->group('reports', ['namespace' => 'App\Controllers'], function($routes) {
+    $routes->post('members/fetchMembers/(:num)', 'Member::fetchMembers/$1');
+
+    $routes->group('reports', ['namespace' => 'App\Controllers'], function ($routes) {
         $routes->get('list/(:segment)', 'Report::list/$1');
     });
-    
+
     $routes->get('reports/details/(:any)', 'Report::viewDetails/$1');
     $routes->post('reports/save_report', 'Report::saveReport');
 });
 
 
-$routes->group('ajax', static function($routes){
-    $routes->post('/','WebController::ajax');
-    $routes->get('(:segment)/(:segment)/(:any)','WebController::ajax/$1/$2/$3');
+$routes->group('ajax', static function ($routes) {
+    $routes->post('/', 'WebController::ajax');
+    $routes->get('(:segment)/(:segment)/(:any)', 'WebController::ajax/$1/$2/$3');
 });
-                     
+
 $routes->get('logout', 'Login::logout');
 $routes->get('/', '\CodeIgniter\Shield\Controllers\LoginController::loginView');
-$routes->get('home','Home::index');
+$routes->get('home', 'Home::index');
 
 service('auth')->routes($routes);
--- app/Controllers/Church/User.php
+++ app/Controllers/Church/User.php
@@ -6,5 +6,195 @@ use App\Controllers\WebController;
 
 class User extends WebController
 {
-    
+    public function viewMyProfile($hashed_id) {
+        $data = [];
+        $numeric_id = hash_id($hashed_id, 'decode');
+
+        if (method_exists($this->model, 'getViewData')) {
+            $data = $this->model->getViewData($numeric_id);
+        } else {
+            $data = $this->model->getOne($numeric_id);
+        }
+
+        $this->parent_id = $hashed_id;
+        $page_data = $this->page_data($data);
+
+        if (method_exists($this->library, 'viewExtraData')) {
+            $this->library->viewExtraData($page_data);
+        }
+
+        if (
+            isset($data) && 
+            is_array($data) && 
+            array_key_exists('id',$data)
+        ) {
+            unset($data['id']);
+        }
+
+        if ($this->request->isAjax()) {
+            return view($this->session->user_type . "/$this->feature/view", $page_data);
+        }
+
+        return view('index', compact('page_data'));
+    }
+
+    public function getAccount($id)
+    {
+        $numeric_id = hash_id($id, 'decode');
+
+        if (method_exists($this->model, 'getViewData')) {
+            $data = $this->model->getViewData($numeric_id);
+        } else {
+            $data = $this->model->getOne($numeric_id);
+        }
+
+        $this->parent_id = $id;
+
+        $page_data = $this->page_data($data);
+
+        if (method_exists($this->library, 'editExtraData')) {
+            // Note the editExtraData updates the $page_data by reference
+            $this->library->editExtraData($page_data);
+        }
+
+        return view($this->session->user_type.'/user/account', $page_data);
+    }
+
+    function passwordVerify()
+    {
+        $user_password = $this->request->getPost('user_password');
+        $user_id = $this->request->getPost('user_id');
+        $numeric_user_id = hash_id($user_id, 'decode');
+
+        $user = $this->model->find($numeric_user_id);
+
+        $validPassword = false;
+        if ($user) {
+            $current_password = addslashes($user['password']);
+            $validPassword = password_verify(trim($user_password), $current_password);
+        }
+
+        return response()->setJSON(['success' => $validPassword]);
+    }
+
+    public function passwordReset($id)
+    {
+        $numeric_id = hash_id($id, 'decode');
+
+        if (method_exists($this->model, 'getEditData')) {
+            $data = $this->model->getEditData($numeric_id);
+        } else {
+            $data = $this->model->getOne($numeric_id);
+        }
+
+        $this->parent_id = $id;
+
+        $page_data = $this->page_data($data);
+
+        if (method_exists($this->library, 'editExtraData')) {
+            // Note the editExtraData updates the $page_data by reference
+            $this->library->editExtraData($page_data);
+        }
+        // return redirect()->to(site_url('users/profile/account' . $hashed_id))->with('message', 'User Private Info updated successfuly!');
+        return view($this->session->user_type.'/user/password_reset', $page_data);
+    }
+
+    public function privacy()
+    {
+        return view($this->session->user_type.'/user/privacy');
+    }
+
+    public function emailNotifications()
+    {
+        return view($this->session->user_type.'/user/email_notification');
+    }
+
+    public function pendingTasks($user_id)
+    {
+        $tasks = [];
+
+        $tasksModel = new \App\Models\TasksModel();
+
+        if ($user_id > 0) {
+            $tasks = $tasksModel->select('tasks.id,tasks.name,tasks.status')
+                ->where('tasks.user_id', hash_id($user_id, 'decode'))
+                ->join('users', 'users.id = tasks.user_id')
+                ->orderBy('tasks.created_at asc')->findAll();
+        } else {
+            $tasks = $tasksModel->select('tasks.id,tasks.name,tasks.status')
+                ->join('users', 'users.id = tasks.user_id')
+                ->orderBy('tasks.created_at asc')->findAll();
+        }
+
+        if (!$tasks) {
+            $page_data['result'] = [];
+        } else {
+            $page_data['result'] = $tasks;
+        }
+
+        $page_data['result'] = $tasks;
+        $page_data['feature'] = 'task';
+        $page_data['action'] = 'list';
+        $page_data['parent_id'] = $user_id;
+
+        // Load the view and pass the data
+        return view($this->session->user_type.'/task/list', $page_data);
+    }
+
+    public function widgets()
+    {
+        return view($this->session->user_type.'/user/widget');
+    }
+
+    public function yourData($id)
+    {
+        $numeric_id = hash_id($id, 'decode');
+
+        if (method_exists($this->model, 'getEditData')) {
+            $user_data = $this->model->getEditData($numeric_id);
+        } else {
+            $user_data = $this->model->getOne($numeric_id);
+        }
+
+        $this->parent_id = $id;
+
+        $page_data = $this->page_data($user_data);
+        if (method_exists($this->library, 'editExtraData')) {
+            // Note the editExtraData updates the $page_data by reference
+            $this->library->editExtraData($page_data);
+        }
+        return view($this->session->user_type.'/user/your_data', $page_data);
+    }
+
+    public function downloadUserDataPdf($user_id)
+    {
+        $user_data = $this->model->getOne($user_id);
+        $user_data = formatUserDataForExport($user_data);
+
+        $this->parent_id = $user_id;
+
+        $userLibrary = new \App\Libraries\UserLibrary();
+        $userLibrary->exportUserDataToPdf($user_data);
+
+        return view($this->session->user_type.'/user/your_data');
+    }
+
+
+    public function deleteAccount()
+    {
+        return view($this->session->user_type.'/user/delete_account');
+    }
+
+    public function deleteMyAccount($userId)
+    {
+        // Delete the user account
+        if ($this->model->delete($userId)) {
+            // Optionally, you can handle session destruction after account deletion
+            session()->destroy();
+
+            return redirect()->to('/')->with('message', 'Account successfully deleted');
+        } else {
+            return redirect()->back()->with('error', 'Failed to delete account');
+        }
+    }
 }
